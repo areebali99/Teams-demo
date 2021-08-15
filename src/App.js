34216663f0ffe5b392ba6e97df961194a8df09b7
Arsalan Ali Shah <Arsalan.Ali@tpsonline.com>
2021-08-16 02:28:02 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route } from 'react-router'
 import { Switch, BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 import Form from './Form'
 
 import TeamForm from './TeamForm'
+const App = () => {
+  const addInfo = (tname, tcategory, tdesc) => {
+    const teamInfo = {
+      tname,
+      tcategory,
+      tdesc
+    }
+    setTeams([...teams, teamInfo])
+    console.log(teamInfo)
+  }
 
-function App () {
+  const [teams, setTeams] = useState([{
+    tname: 'Team A',
+    tcategory: 'category A',
+    tdesc: 'this is development team'
+  }
+  ])
+
   return (
     <>
 
@@ -14,7 +30,9 @@ function App () {
         <Switch>
           <Route exact path="/" component={Form} />
 
-          <Route exact path="/teamform" component={TeamForm} />
+          <Route exact path="/teamform" component={TeamForm} >
+            <TeamForm addInfo={addInfo} />
+          </Route>
         </Switch>
       </Router>
 
